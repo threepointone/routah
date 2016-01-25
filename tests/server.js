2ff48b6655f2a95a656645b68a8eaf60bce9940f
Sunil Pai <threepointone@gmail.com>
2016-01-25 16:43:53 +0530

    stubs for server side tests

--- tests/server.js
+++ tests/server.js
@@ -1,7 +1,49 @@
 /* global describe, it */
+import React, {Component, PropTypes} from 'react';
+import {Router, Route} from '../src';
+import {renderToString, renderToStaticMarkup} from 'react-dom/server';
+import {createMemoryHistory} from 'history';
+
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+
 describe('server side routah', () => {
-  it('should render to string');
+  it('prop: history', done => {
+    class App extends Component{
+      static contextTypes = {
+        history: PropTypes.object
+      };
+      componentWillMount(){
+        this.context.history.listen(l => expect(l.pathname).toEqual('/xyz'))();
+        done();
+      }
+      render(){
+        return null;
+      }
+    }
+
+    renderToString(<Router history={createMemoryHistory('/xyz')}><App/></Router>);
+  });
+  it('prop: url', done => {
+    class App extends Component{
+      static contextTypes = {
+        history: PropTypes.object
+      };
+      componentWillMount(){
+        this.context.history.listen(l => expect(l.pathname).toEqual('/xyz'))();
+        done();
+      }
+      render(){
+        return null;
+      }
+    }
+
+    renderToString(<Router url='/xyz'><App/></Router>);
+
+  });
+  it('should render to string/markup');
   it('should catch redirects');
-  it('should handle custom history objects');
-  it('prop: url');
+
+
 });
